cd7c0979df7c5e746b68eedc99facd6b1b43ca3c
Giovanni Carlo Marasco <mexitalian@gmail.com>
2019-08-15 16:41:35 +0200

    adds the plaback permission request to the scopes, adds a boilerplate folder which will be the jumping off point from now on, const endpoints are beginning to be populated

---
+++ const.js
@@ -0,0 +1,5 @@
+const BASE_URL = "https://api.spotify.com/v1"
+exports.BASE_URL = BASE_URL;
+exports.USERS = `${BASE_URL}/users`; // put
+exports.PAUSE = `${BASE_URL}/me/player/pause`; // put
+exports.PLAY = `${BASE_URL}/me/player/play`; // put
